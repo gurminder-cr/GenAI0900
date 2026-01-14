f9c34038361693abc142c082a8466aca3fdb954c
gurminder-cr <gurminder@coderroots.com>
2026-01-14 10:35:10 +0530

    Question update

--- Question.py
+++ Question.py
@@ -7,4 +7,7 @@ for i in range(0,len(name)):
 # Write a program to find the sum of first n natural numbers(n is integer input from user)
 # Print the multiplication table of a given number using a for loop
 # output - 2*2=4 
+# Write a program to find the sum of first n natural numbers(n is integer input from user)
+# Print the multiplication table of a given number using a for loop
+# output - 2*2=4 
 
