092863d479f8cd1207b055faf0d9d8c910220896
gurminder-cr <gurminder@coderroots.com>
2026-01-23 10:25:09 +0530

    Lambda Enumerate

---
+++ 12lambda.py
@@ -0,0 +1,41 @@
+# default parameters 
+# def newFunc(a,b=5):
+#     print(a+b)
+    
+# newFunc(12,10)
+
+
+# lambda functions - anonymous function - single line function
+  # lambda (parameters): print() or return 
+# s= lambda a,b:a+b
+# s= lambda a,b:print(a+b)
+# print(s(10,12))
+
+
+# def oddNumber(a):
+#     if a%2!=0:
+#         # return "Odd"
+#         print("Odd")
+#     else:
+#         print("Even")
+#         # return "Even"
+# oddNumber=lambda a:"Odd" if a%2!=0 else "Even"
+# print(oddNumber(12))
+
+
+# Enumerate function 
+l=[12,35,11,10,4,32]
+# print(l)
+# for i in enumerate(l):
+#     print(i)
+# for i,j in enumerate(l):
+#     print(i,"--",j)
+
+di= {'name':'Sourav','class':'btech','rollno':123}
+for i in di.items():
+    print(i)
+for i,j in enumerate(di.items()):  # .keys or .values
+    print(i,"--",j)
+    
+    
+    
