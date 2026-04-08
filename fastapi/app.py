8f7a40a700184ab69fe8b73ccd64ae033ba57944
gurminder-cr <gurminder@coderroots.com>
2026-04-08 10:16:09 +0530

    Heart Model API

--- fastapi/app.py
+++ fastapi/app.py
@@ -1,10 +1,11 @@
 # pip install uvicorn fastapi 
 import uvicorn 
 from fastapi import FastAPI
-from input import getData
+from input import getData, heartInput
+import joblib
 
 app =FastAPI()
-    
+model= joblib.load('model.pkl')    
 # get, post, update, delete 
 @app.get('/')
 def index():
@@ -28,7 +29,32 @@ def getName(data:getData):
     
     return dict_data
 
-
+@app.post('/heartdisease')
+def heartDis(data: heartInput):
+    data= data.model_dump()
+    age=data['age']
+    sex=data['sex']
+    cp= data['cp']
+    trestbps= data['trestbps'] 
+    chol= data['chol']
+    fbs = data['fbs']
+    restecg= data['restecg']
+    thalach= data['thalach']
+    exang= data['exang']
+    oldpeak=data['oldpeak']
+    slope= data['slope']
+    ca= data['ca']
+    thal= data['thal']
+    
+    host_data= [age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]
+    pred_result = model.predict([host_data])
+    
+    if pred_result[0]==1:
+        prediction= "Heart Disease Detected"
+    else:
+        prediction="No Heart Disease"
+        
+    return prediction
 
 
 # to run fastapi - uvicorn filename:objectname --reload
