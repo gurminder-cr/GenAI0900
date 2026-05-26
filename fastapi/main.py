06d03475efa07cacca8beac1e8f78aa2dac3e1ec
gurminder-cr <gurminder@coderroots.com>
2026-05-26 13:33:24 +0530

    change

--- fastapi/main.py
+++ fastapi/main.py
@@ -11,9 +11,13 @@ def index():
 @app.get('/name')
 def printNaam():
     return {'message':'Hello Rohitpreet'}
+@app.post('/new')
+def printNew():
+    return {'message':'Hello Sahil'}
 
 
 if __name__=="__main__":
     uvicorn.run(app,host='127.0.0.1',port=8000)
+    
     
 # uvicorn file_name:objectname --reload 
